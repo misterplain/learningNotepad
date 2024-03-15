14021ded5475633c24cffbcc7802b46438a87d67
Patrick O'Brien <obrienp2321@gmail.com>
2024-03-15 16:49:20 +0100

    recursion capitalize first

--- DataStructures&Algorithms/Algorithms/Recursion/General Recursion/Easy/CapitalizeFirst.js
+++ DataStructures&Algorithms/Algorithms/Recursion/General Recursion/Easy/CapitalizeFirst.js
@@ -2,10 +2,7 @@
 // Write a recursive function called capitalizeFirst. Given an array of strings, capitalize the first letter of each string in the array.
 
 function capitalizeFirst(array) {
-  if (array.length === 0) {
-    return;
-  }
-  return [capitalizeFirst];
+
 }
 
 console.log(capitalizeFirst(["car", "taco", "banana"])); // ['Car','Taco','Banana']
