9b275702a56d62e55c90ec5731dca6259e08bd0c
Patrick O'Brien <obrienp2321@gmail.com>
2024-04-23 20:14:19 +0200

    dsa/ds/hashTable/overview

--- DataStructures&Algorithms/DataStructures/HashTable/Overview.js
+++ DataStructures&Algorithms/DataStructures/HashTable/Overview.js
@@ -44,6 +44,116 @@
 const a = new Map();
 const b = new Set();
 
+//creating a hashtable based on DSA course
+//what makes a good hash
+//fast( constant time)
+//doesn't cluster outputs at specific indices, but distributes uniformly
+//deterministic - same input yields same output
+
+//hash function with just strings
+hash("pink");
+function hash(key, arrayLen) {
+  let total = 0;
+  for (let char of key) {
+    //map a to 1, b to 2
+    let value = char.charCodeAt(0) - 96;
+    total = (total + value) % arrayLen;
+  }
+  return total;
+}
+
+//improved version
+function hash(key, arrayLen) {
+  let total = 0;
+  let WEIRD_PRIME = 0;
+  for (let i = 0; i < Math.min(key.length, 100); i++) {
+    let char = key[i];
+    let value = char.charCodeAt(0) - 96;
+    total = (total * WEIRD_PRIME + value) % arrayLen;
+  }
+  return total;
+}
+
+//from course
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
+  }
+
+  _hash(key) {
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+    }
+    return total;
+  }
+  set(key, value) {
+    let index = this._hash(key);
+    if (!this.keyMap[index]) {
+      this.keyMap[index] = [];
+    }
+    this.keyMap[index].push([key, value]);
+  }
+  get(key) {
+    let index = this._hash(key);
+    if (this.keyMap[index]) {
+      for (let i = 0; i < this.keyMap[index].length; i++) {
+        if (this.keyMap[index][i][0] === key) {
+          return this.keyMap[index][i][1];
+        }
+      }
+    }
+    return undefined;
+  }
+  keys() {
+    let keysArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!keysArr.includes(this.keyMap[i][j][0])) {
+            keysArr.push(this.keyMap[i][j][0]);
+          }
+        }
+      }
+    }
+    return keysArr;
+  }
+  values() {
+    let valuesArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!valuesArr.includes(this.keyMap[i][j][1])) {
+            valuesArr.push(this.keyMap[i][j][1]);
+          }
+        }
+      }
+    }
+    return valuesArr;
+  }
+}
+
+let ht = new HashTable(17);
+ht.set("maroon", "#800000");
+ht.set("yellow", "#FFFF00");
+ht.set("olive", "#808000");
+ht.set("salmon", "#FA8072");
+ht.set("lightcoral", "#F08080");
+ht.set("mediumvioletred", "#C71585");
+ht.set("plum", "#DDA0DD");
+ht.set("purple", "#DDA0DD");
+ht.set("violet", "#DDA0DD");
+
+ht.keys().forEach(function (key) {
+  console.log(ht.get(key));
+});
+
+
+/**------------------------------------------------------------------- */
+
 //create a new hash table
 //-Insert, lookup, delete, and search are all O(1) time complexity
 //          -Collission - when two keys map to the same index in an array, this
@@ -56,6 +166,7 @@ class HashTable {
   _hash(key) {
     let hash = 0;
     for (let i = 0; i < key.length; i++) {
+      //end up with a number that is valid index that'll be less than the index
       hash = (hash + key.charCodeAt(i) * i) % this.data.length;
       console.log(hash);
     }
@@ -149,4 +260,4 @@ function firstRecurringCharacter2(input) {
 
 firstRecurringCharacter2([2, 5, 1, 2, 3, 5, 1, 2, 4]);
 
-//can put in difference types of data 
+//can put in difference types of data
